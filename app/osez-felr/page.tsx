199fc72a86ec0bca62f82b75f3a0d323baddaf2f
Samuel <samuel.jaglale-dalleau@epitech.eu>
2025-11-16 13:54:09 +0400

    add the form

--- app/osez-felr/page.tsx
+++ app/osez-felr/page.tsx
@@ -1,10 +1,18 @@
-import SlidingForms from "@/src/components/slidingForm";
+// app/ta-route/page.tsx (là où est déjà ton fichier)
 
-export default function page() {
-    return (
-        <div>
-            
-            <SlidingForms />
-        </div>
-    );
+const SPHINX_URL =
+  "https://s2.sphinxonline.net/SurveyServer/s/OptiSurvey/FELR-JIE-2025/Quest.htm?ORIGINE_SAISIE=Iframe";
+
+export default function Page() {
+  return (
+    <div className="flex-1">
+      <iframe
+        src={SPHINX_URL}
+        title="Formulaire d'inscription FELR"
+        className="w-full h-[90vh] border-0 rounded-xl bg-white"
+        loading="lazy"
+      />
+    </div>
+  );
 }
+
